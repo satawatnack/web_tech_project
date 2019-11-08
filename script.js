e3b18014d863cccdbfaab4cb9e52146de14568ca
satawatnack <60070093@kmitl.ac.th>
2019-11-09 00:12:36 +0700

    update

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ console.log(prevScrollpos);
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     if (currentScrollPos == 0) {
-        document.getElementsByTagName("nav")[0].style.backgroundColor = ("background-color", "rgb(34, 2, 0, 0)");
+        document.getElementsByTagName("nav")[0].style.backgroundColor = ("background-color", "rgb(45, 45, 47, 0)");
     } else if (prevScrollpos > currentScrollPos) {
-        document.getElementsByTagName("nav")[0].style.backgroundColor = ("background-color", "rgb(34, 2, 0, 0.8)");
+        document.getElementsByTagName("nav")[0].style.backgroundColor = ("background-color", "rgb(45, 45, 47, 0.8)");
         document.getElementsByTagName("nav")[0].style.top = "0";
     } else {
         document.getElementsByTagName("nav")[0].style.top = "-90px";
     }
     prevScrollpos = currentScrollPos;
 }
+AOS.init();
