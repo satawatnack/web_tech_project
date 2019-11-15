e09c2ab01c37f20ad1c6bf608b06c5e2c295e8b4
warinsukanthmet <60070089@kmitl.ac.th>
2019-11-15 14:19:47 +0700

    real final

--- script-for-content.js
+++ script-for-content.js
@@ -4,14 +4,14 @@ window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     if (currentScrollPos == 0) {
         document.getElementsByTagName("nav")[0].style.backgroundColor = ("background-color", "rgb(45, 45, 47, 0)");
-        document.getElementById("logo").style.color = ("color","black");
+        document.getElementById("alinkid").style.color = ("color","black");
         document.getElementById("about").style.color = ("color", "black");
         document.getElementById("park").style.color = ("color", "black");
         document.getElementById("home").style.color = ("color", "black");
     } else if (prevScrollpos > currentScrollPos) {
         document.getElementsByTagName("nav")[0].style.backgroundColor = ("background-color", "rgb(45, 45, 47, 0.8)");
         document.getElementsByTagName("nav")[0].style.top = "0";
-        document.getElementById("logo").style.color = ("color", "white");
+        document.getElementById("alinkid").style.color = ("color", "white");
         document.getElementById("about").style.color = ("color", "white");
         document.getElementById("park").style.color = ("color", "white");
         document.getElementById("home").style.color = ("color", "white");
